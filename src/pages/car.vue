7703f5da58afbfd79dfcbd5c78c0cca65c1318b8
陈黎明 <275298711@qq.com>
2019-09-17 17:46:16 +0800

    init

---
+++ src/pages/car.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="about">
+    <h1>car</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "./pages/Home.vue";
+import Car from "./pages/car.vue";
+const About = () => import(/* webpackChunkName: "about" */ "./pages/About.vue")
 
 Vue.use(Router);
 
@@ -14,13 +16,16 @@ export default new Router({
       component: Home
     },
     {
+      path: "/car",
+      name: "car",
+      component: Car
+    },
+    {
       path: "/about",
       name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
     }
   ]
 });
